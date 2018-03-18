60353cd7fbe12aeb85046305b690ddc5de767be5
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-03-17 20:39:36 -0700

    Building FinishGoalVC / Passing Data from CreateGoalVC slight bug for dismissing keyboard view on CreateGoalVC

---
+++ Goal Posting/Controller/FinishGoalVC.swift
@@ -0,0 +1,70 @@
+//
+//  FinishGoalVC.swift
+//  Goal Posting
+//
+//  Created by Nathaniel Burciaga on 3/17/18.
+//  Copyright © 2018 Nathaniel Burciaga. All rights reserved.
+//
+
+import UIKit
+
+class FinishGoalVC: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var createGoalBtn: UIButton!
+    @IBOutlet weak var pointsTextField: UITextField!
+    
+    var goalDescription: String!
+    var goalType: GoalType!
+    
+    
+    func initData(description: String, type: GoalType) {
+        self.goalDescription = description
+        self.goalType = type
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        createGoalBtn?.bindToKeyboard()
+        self.hideKeyboardWhenTappedAround()
+        pointsTextField?.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func createGoalBtnPressed(_ sender: Any) {
+        // Pass data into Core Data View Model
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
